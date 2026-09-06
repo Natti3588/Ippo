9ef89c208854091febee221b87b0742adb2e7ef2
Natti3588 <tnbox3588git@gmail.com>
2026-09-06 21:49:43 +0900

    feat(backend): ドメイン層のトピックにソートを追加

--- backend/internal/domain/topic.go
+++ backend/internal/domain/topic.go
@@ -1,5 +1,13 @@
 package domain
 
+type SortOrder string
+
+const (
+	SortPopular SortOrder = "popular"
+	SortNewest  SortOrder = "newest"
+	SortOldest  SortOrder = "oldest"
+)
+
 type Topic struct {
 	Id   string
 	Slug string
