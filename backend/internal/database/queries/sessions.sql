6f96a1b49ad143d487f2e960b82d662d75aa5731
Natti3588 <tnbox3588git@gmail.com>
2026-09-08 20:58:46 +0900

    feat(backend): セッションのクエリを追加

---
+++ backend/internal/database/queries/sessions.sql
@@ -0,0 +1,19 @@
+-- name: CreateSession :exec
+INSERT INTO sessions (id, user_id, expires_at)
+VALUES (?, ?, ?);
+
+-- name: GetSessionWithUser :one
+SELECT
+  s.id AS session_id,
+  s.expires_at AS expires_at,
+  u.id AS user_id,
+  u.display_name AS display_name,
+  u.email AS email
+FROM sessions s
+JOIN users u ON s.id = u.id
+WHERE s.id = ?
+AND s.expires_at > sqlc.arg(now);
+
+-- name: DeleteSession :exec
+DELETE FROM sessions
+WHERE id = ?;
